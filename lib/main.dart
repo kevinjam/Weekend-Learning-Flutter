d4af27eac49d61bc40471e2f708a5e91ad4774aa
kevin janvier <kevin.janvier5@gmail.com>
2020-05-10 13:41:04 +0300

    learn text

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/app_screens/first_screen.dart';
 
-void main()=> runApp(new MyFlutterApp());
+import 'app_screens/home.dart';
 
-
-class MyFlutterApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "Weekend Flutter",
-        home:Scaffold(
-          appBar: AppBar(title: Text("My First Application"),),
-          body:FirstScreen()
-        )
-    );
-  }
-
-
+void main(){
+  runApp(
+    MaterialApp(
+      title: "Explore Widgets",
+      home: Home(),
+    )
+  );
 }
